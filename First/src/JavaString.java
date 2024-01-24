ebdbdd57f46095a6f8057a46c17e686fc1185d8d
AHNayef <ahnayef@outlook.com>
2024-01-25 00:38:25 +0600

    Fix java error

--- First/src/JavaString.java
+++ First/src/JavaString.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class JavaString {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            String name = "Ahn";
 
-        String name = "Ahn";
+            System.out.println(name.toUpperCase());
+            System.out.println(name.toLowerCase());
+            System.out.println(name.compareTo("AHn"));
 
-        System.out.println(name.toUpperCase());
-        System.out.println(name.toLowerCase());
-        System.out.println(name.compareTo("AHn"));
+            String greetName = sc.nextLine();
 
-        String greetName = sc.nextLine();
-
-        System.out.println("Hello, " + greetName);
+            System.out.println("Hello, " + greetName);
+        }
 
     }
 }
