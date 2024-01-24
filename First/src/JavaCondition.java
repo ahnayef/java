10079897c43ceba5b3821ef4099c8d3c12124e67
AHNayef <ahnayef@outlook.com>
2024-01-25 00:47:34 +0600

    Java Condition

---
+++ First/src/JavaCondition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JavaCondition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a >= b) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
