3e8a5c2d6dc034ff8ba6ab607a093d550712f3ff
AHNayef <ahnayef@outlook.com>
2024-01-25 00:48:09 +0600

    Fix java error

--- First/src/JavaCondition.java
+++ First/src/JavaCondition.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class JavaCondition {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
-        if (a >= b) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
+            if (a >= b) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
         }
     }
 }
