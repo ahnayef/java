f8ac31eca798b0382eede9ac56f8a0270bb84261
AHNayef <ahnayef@outlook.com>
2024-01-25 00:37:36 +0600

    Java String

---
+++ First/src/JavaString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JavaString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = "Ahn";
+
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.compareTo("AHn"));
+
+        String greetName = sc.nextLine();
+
+        System.out.println("Hello, " + greetName);
+
+    }
+}
