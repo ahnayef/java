c62cf03ca22d9360b2154a20a45dc6b8c075f886
AHNayef <ahnayef@outlook.com>
2024-02-04 22:38:46 +0600

    Java loop and Array

---
+++ Practice/Array/Array.java
@@ -0,0 +1,27 @@
+package Practice.Array;
+
+import java.util.Scanner;
+
+public class Array {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array length: ");
+        int len = sc.nextInt();
+        int arr[] = new int[len];
+
+        System.out.print("Enter " + len + " element: ");
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+    }
+
+}
