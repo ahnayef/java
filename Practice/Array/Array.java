a8f5cca6d6288ce53bde1349f09a71b8104590d0
AHNayef <ahnayef@outlook.com>
2024-02-04 22:39:54 +0600

    Fix

--- Practice/Array/Array.java
+++ Practice/Array/Array.java
@@ -6,20 +6,20 @@ public class Array {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter array length: ");
+            int len = sc.nextInt();
+            int arr[] = new int[len];
 
-        System.out.print("Enter array length: ");
-        int len = sc.nextInt();
-        int arr[] = new int[len];
+            System.out.print("Enter " + len + " element: ");
 
-        System.out.print("Enter " + len + " element: ");
+            for (int i = 0; i < len; i++) {
+                arr[i] = sc.nextInt();
+            }
 
-        for (int i = 0; i < len; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        for (int i : arr) {
-            System.out.println(i);
+            for (int i : arr) {
+                System.out.println(i);
+            }
         }
 
     }
